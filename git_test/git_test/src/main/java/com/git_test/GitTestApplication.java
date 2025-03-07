01547ce63ba076eddf98d9de167beb66c24a2fdb
ashlesa <ashlesasukla1114@gmail.com>
2025-03-07 15:00:04 +0530

    first commit using ide

---
+++ git_test/git_test/src/main/java/com/git_test/GitTestApplication.java
@@ -0,0 +1,15 @@
+package com.git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+	int x=12;
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestApplication.class, args);
+	}
+
+}
