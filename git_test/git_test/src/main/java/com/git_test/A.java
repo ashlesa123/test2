16ffa3deda25ce2eda137c70bcfce10be906a671
ashlesa123 <ashlesasukla1114@gmail.com>
2025-03-09 13:34:41 +0530

    Update A.java
    added some code

--- git_test/git_test/src/main/java/com/git_test/A.java
+++ git_test/git_test/src/main/java/com/git_test/A.java
@@ -4,8 +4,13 @@ public class A {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello git!!");
-  }
-                          
+    for(int i=0;i<10;i++) {
+      if(i %2==0) {
+        System.out.print(i);
+        
+
+     }                      
+}
+  }                         
 }
 
